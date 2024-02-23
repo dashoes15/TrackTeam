a6698b102696890d0ea19ba45d1c87ed4e55a8b4
dashoes15 <daltonasali@gmail.com>
2024-02-23 09:16:12 -0500

    day 3

--- src/TrackTeam.java
+++ src/TrackTeam.java
@@ -1,8 +1,36 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class TrackTeam {
     public static void main(String[] args) {
-        //Create coach objects
+        ArrayList<Coach> coaches = new ArrayList<Coach>();
+        ArrayList<Thrower> throwers = new ArrayList<Thrower>();
+
+        //Create objects
         Coach c1 = new Coach("Emory", "George", "Throws", false);
         Coach c2 = new Coach("John", "Jarvis", "Distance", true);
+
+        Thrower t1 = new Thrower("Jason", "Ocampo", 12, 1000000, 31, 97);
+        Thrower t2 = new Thrower("Dalton", "Sali", 12, 0, 27, 86);
+
+        //Add objects to coaches ArrayList
+        coaches.add(c1);
+        coaches.add(c2);
+
+        throwers.add(t1);
+        throwers.add(t2);
+
+        //Display
+        System.out.println("Track Coaches:");
+        for(Coach c: coaches){
+            System.out.println(c);
+            System.out.println("\n--------------------------");
+        }
+        System.out.println("Throwers: ");
+        for(Thrower t: throwers){
+            System.out.println(t);
+            System.out.println("\n----------------------");
+        }
 
         //Outputs
         System.out.println(c1); // Automatically finds to-string
